7f9371c01feb8c154677afcd8c8c3616263bb86d
Daniel Leitner <dmcleite@hotmail.com>
2018-05-01 10:31:47 +0200

    implemented variableparser

--- CSP/CSP/VariableParser.cs
+++ CSP/CSP/VariableParser.cs
@@ -10,7 +10,22 @@ namespace CSP
 	{
 		public static List<string> Parse(string input)
 		{
-			return new List<string> {input};
+			var variables = input.Split(',').ToList();
+			for (var i = 0; i < variables.Count; i++)
+			{
+				variables[i] = variables[i].Trim();
+				variables[i] = variables[i].Replace(' ', '_');
+			}
+
+			foreach (var variable in variables)
+			{
+				if(string.IsNullOrEmpty(variable))
+					throw new ArgumentException("Variable name must not be empty!");
+				var cnt = variables.Count(x => x == variable);
+				if(cnt > 1)
+					throw new ArgumentException("Variables are not unique!");
+			}
+			return variables;
 		}
 	}
 }
--- CSP/CSP_Unittests/VariableParserTests.cs
+++ CSP/CSP_Unittests/VariableParserTests.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ApprovalTests;
+using ApprovalTests.Reporters;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CSP;
 
 namespace CSP_Unittests
 {
 	[TestClass]
+	[UseReporter(typeof(WinMergeReporter))]
 	public class VariableParserTests
 	{
 		[TestMethod]
@@ -18,6 +20,78 @@ namespace CSP_Unittests
 			var input = "a";
 			var output = VariableParser.Parse(input);
 			Approvals.VerifyAll(output, "Variable");
+		}
+
+		[TestMethod]
+		public void VerifyStringWithTwoVariables()
+		{
+			var input = "a,b";
+			var output = VariableParser.Parse(input);
+			Approvals.VerifyAll(output, "Variable");
+		}
+
+		[TestMethod]
+		public void VerifyStringWithUntrimmedVariables()
+		{
+			var input = " a,b ";
+			var output = VariableParser.Parse(input);
+			Approvals.VerifyAll(output, "Variable");
+		}
+
+		[TestMethod]
+		public void WhenVariableHasWhiteSpace_ThenReplaceWithUnderline()
+		{
+			var input = " a,b c";
+			var output = VariableParser.Parse(input);
+			Approvals.VerifyAll(output, "Variable");
+		}
+
+		[TestMethod]
+		public void WhenVariablesNotUnique_ThenThrowException()
+		{
+			var input = " a,a";
+			var exc = "";
+			try
+			{
+				var output = VariableParser.Parse(input);
+			}
+			catch (ArgumentException ex)
+			{
+				exc = ex.Message;
+			}
+			Approvals.Verify("Exception message: " +exc);
+		}
+
+		[TestMethod]
+		public void WhenVariableLengthIsNull_ThenThrowException()
+		{
+			var input = " a,";
+			var exc = "";
+			try
+			{
+				var output = VariableParser.Parse(input);
+			}
+			catch (ArgumentException ex)
+			{
+				exc = ex.Message;
+			}
+			Approvals.Verify("Exception message: " + exc);
+		}
+
+		[TestMethod]
+		public void WhenInputIsEmpty_ThenThrowException()
+		{
+			var input = "";
+			var exc = "";
+			try
+			{
+				var output = VariableParser.Parse(input);
+			}
+			catch (ArgumentException ex)
+			{
+				exc = ex.Message;
+			}
+			Approvals.Verify("Exception message: " + exc);
 		}
 	}
 }
